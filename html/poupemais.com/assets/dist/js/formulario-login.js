d4b8bbea9dbdf50dd76dfb07ce2d3fee53f4ab00
Fernando Estevam <fernandoestevam23@gmail.com>
2020-02-21 20:13:18 -0300

    Iniciando o script Ajax login

--- html/poupemais.com/assets/dist/js/formulario-login.js
+++ html/poupemais.com/assets/dist/js/formulario-login.js
@@ -4,11 +4,29 @@ const frmLogin = document.querySelector('#form-login');
 
 const form = {
   login: document.querySelector('#login'),
-  passwd: document.querySelector('#passwd'),
+  passwd: document.querySelector('#password'),
 }
 
-function sendlogin(){
-  
+function sendlogin(e){
+  e.preventDefault();
+
+  const url = frmLogin.action;
+  const dados = `login=${form.login.value}&password=${form.passwd.value}`;
+  const xhr = new XMLHttpRequest();
+  xhr.open('POST', url, true);
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  xhr.onload = handler;
+  xhr.response
+  xhr.send(dados);
+}
+
+function handler() {
+  this.onreadystatechange = function() {
+    if(this.status == 200 && this.readState == 4) {
+      let responseJson = JSON.parse(this.responseText);
+      console.log(reponseJson);
+    }
+  }
 }
 
 frmLogin.addEventListener('submit', sendlogin, false);
