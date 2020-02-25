7a6786fc2be1afcb292b3a1e8ec720e2e91e218b
Fernando Estevam <fernandoestevam23@gmail.com>
2020-02-25 09:16:53 -0300

    adicionando atributo aporte, ajustando vencimentos

--- html/poupemais.com/Src/Lib/Investimento.php
+++ html/poupemais.com/Src/Lib/Investimento.php
@@ -14,20 +14,26 @@ class Investimento extends ValidaDados
   private string $plano;
   private float $valorPlano;
   private array $vencimento;
+  private int $aporte;
+  private string $dataPrimeiraParcela;
   
-  public function __construct(string $plano, float $valorPlano, string $dataPrimeiraParcela=null)
+  public function __construct(string $plano, float $valorPlano, int $aporte, string $dataPrimeiraParcela)
   {
     $this->plano = $this->converInt($plano);
-    $this->valorPlano = $valorPlano;
-    $this->vencimento = $this->vencimentos($this->plano, $this->valorPlano, $dataPrimeiraParcela);
+    $this->valorPlano = $this->validaInput($valorPlano);
+    $this->dataPrimeiraParcela = $this->validaInput($dataPrimeiraParcela);
+    $this->aporte = $this->validaInput($aporte);
+    $this->vencimento = $this->vencimentos($this->plano, $this->valorPlano, $this->aporte, $this->dataPrimeiraParcela);
 
   }
     
   # Cria vencimentos baseado no plano e valor
-  private function vencimentos(int $nParcelas, float $v, string $dataPrimeiraParcela=null): array 
+  private function vencimentos(int $nParcelas, float $v, int $aporte, string $dataPrimeiraParcela): array 
   {
-    if(!isset($nParcelas) || !isset($v)) {
-      if(empty($nParcelas) || empty($v))
+    if(!isset($nParcelas) || !isset($v) || !isset($aporte)) {
+      if(empty($nParcelas) || empty($v) || empty($aporte)) {
+        Erro::setErro("Informar número de parcelas, valor e data da primeira parcela");
+      }
       Erro::setErro("Informar número de parcelas, valor e data da primeira parcela");
     }
 
@@ -35,20 +41,22 @@ class Investimento extends ValidaDados
 
     $parcela = ['A','B','C','D','E','F','G','H','I','J','K','L'];
     
-    if($dataPrimeiraParcela != null){
-        $dia = $dataPrimeiraParcela;
-        $mes = date("m");
-        $ano = date("Y");
-    } else {
-        $dia = date("d");
-        $mes = date("m");
-        $ano = date("Y");
-    }
+    if($aporte === 2) $nParcelas = 1;
+
+    // if($dataPrimeiraParcela != null){
+      $dia = $dataPrimeiraParcela;
+      $mes = date("m");
+      $ano = date("Y");
+    // } else {
+      // $dia = date("d");
+      // $mes = date("m");
+      // $ano = date("Y");
+    // }
     
     for($x = 0; $x < $nParcelas; $x++){
-        $dado = date("Y/m/d",strtotime("+".$x." month",mktime(0, 0, 0,$mes,$dia,$ano)));
-        $valor = $v; 
-        array_push($vencimentos, ['parcela' => $parcela[$x] , 'vencimento' => $dado ,'valor'=>$valor]); 
+      $dado = date("Y/m/d",strtotime("+".($x+1)." month",mktime(0, 0, 0,$mes,$dia,$ano)));
+      $valor = $v; 
+      array_push($vencimentos, ['parcela' => $parcela[$x] , 'vencimento' => $dado ,'valor'=>$valor]); 
     }
     return $vencimentos;
   }
