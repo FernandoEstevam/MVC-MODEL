6e78f1c32b1a1d206421ee5c6094f1c560e804c6
Fernando Estevam <fernandoestevam23@gmail.com>
2020-02-25 21:54:34 -0300

    criando conexao banco de dados

---
+++ html/poupemais.com/Src/Core/ConexaoDB.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * class PDO - Conexao Banco de dados
+ */
+namespace Poupemais\Src\Core;
+use PDO;
+use PDOException;
+
+class ConexaoDB
+{
+  public function __construct() 
+  {
+    try {
+      $this->conn = new PDO(
+        DATA_LAYER_CONFIG['driver'].":host=".DATA_LAYER_CONFIG['host'].";dbname=".DATA_LAYER_CONFIG['dbname'],
+        DATA_LAYER_CONFIG['username'],
+        DATA_LAYER_CONFIG['passwd'],
+        DATA_LAYER_CONFIG['options']
+      );
+    } catch (PDOException $e) {
+      echo $e->getMessage();
+    }
+  }
+}
