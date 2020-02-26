02fadfc6510e4d8178d7111e2ab7457e200e349a
Fernando Estevam <fernandoestevam23@gmail.com>
2020-02-25 23:35:54 -0300

    Criando class crud para consulta banco de dados

---
+++ html/poupemais.com/Src/Lib/Crud.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * class CRUD
+ */
+namespace Poupemais\Src\Lib;
+use Poupemais\Src\Core\ConexaoDB;
+
+class Crud extends ConexaoDB
+{
+  private $query;
+
+  # Executa a consulta banco de dados
+  public function prepareExec(string $prep, array $exec): void
+  {
+    $this->query = $this->conn->prepare($prep);
+    $this->query->execute($exec);
+  }
+
+  # Select banco de dados
+  public function selectDB(string $fields, string $table, string $where, array $exec)
+  {
+    $this->prepareExec("SELECT {$fields} FROM {$table} {$where}", $exec);
+    return $this->query;
+  }
+}
