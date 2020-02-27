81db7b8463c2e9244f268eff002586b5956627fa
Fernando Estevam <fernandoestevam23@gmail.com>
2020-02-27 14:41:00 -0300

    Comentando testes

--- html/poupemais.com/App/Controllers/HomeController.php
+++ html/poupemais.com/App/Controllers/HomeController.php
@@ -33,52 +33,54 @@ class HomeController extends Controller
 
   public function cadastrar(): void
   {
-    $_POST['login'] = "fernandoestevam@gmail.com";
-    $_POST['conf_email'] = "fenranoestevam@gmail.com";
-    $_POST['passwd'] = "odnanref709244";
-    $_POST['confPasswd'] = "odnanref709244";
-    $_POST['nome'] = "fernando antonio estevam";
-    $_POST['cpf'] = "323.322.808-20";
-    $_POST['rg'] = "40.633.778-0";
-    $_POST['dataNasc'] = '23/07/1985';
-    $_POST['estadoCivil'] = "casado";
-    $_POST['telefone'] = "(11) 9.9582-1424";
-    $_POST['cep'] = "08253-655";
-    $_POST['rua'] = 'Rua Jacaré-Copaiba';
-    $_POST['numero'] = '171';
-    $_POST['bairro'] = 'Vila Marina';
-    $_POST['cidade'] = 'São Paulo';
-    $_POST['uf'] = 'sp';
-    $_POST['plano'] = 6;
+    // $_POST['login'] = "fernandoestevam@gmail.com";
+    // $_POST['conf_email'] = "fenranoestevam@gmail.com";
+    // $_POST['passwd'] = "odnanref709244";
+    // $_POST['confPasswd'] = "odnanref709244";
+    // $_POST['nome'] = "fernando antonio estevam";
+    // $_POST['cpf'] = "323.322.808-20";
+    // $_POST['rg'] = "40.633.778-0";
+    // $_POST['dataNasc'] = '23/07/1985';
+    // $_POST['estadoCivil'] = "casado";
+    // $_POST['telefone'] = "(11) 9.9582-1424";
+    // $_POST['cep'] = "08253-655";
+    // $_POST['rua'] = 'Rua Jacaré-Copaiba';
+    // $_POST['numero'] = '171';
+    // $_POST['bairro'] = 'Vila Marina';
+    // $_POST['cidade'] = 'São Paulo';
+    // $_POST['uf'] = 'sp';
+    // $_POST['plano'] = 6;
 
-    $input = $this->arrayInputs($_POST);
+    // $input = $this->arrayInputs($_POST);
 
-    $this->cliente = new Cliente(
-      new Usuario(
-        $input['login'], 
-        $input['passwd'], null, $input['confPasswd']), 
-      new Endereco($input['cep'], 
-        $input['rua'], 
-        $input['numero'],
-        $input['bairro'], 
-        $input['cidade'], 
-        $input['uf']),
-      new CPF($input['cpf']),
-      $input['nome'],
-      $input['rg'],
-      $input['dataNasc'],
-      $input['estadoCivil'],
-      $input['telefone'],
-      new Investimento(
-      "6 meses",
-      200.00)
-    );
+    // $this->cliente = new Cliente(
+    //   new Usuario(
+    //     $input['login'], 
+    //     $input['passwd'], null, $input['confPasswd']), 
+    //   new Endereco($input['cep'], 
+    //     $input['rua'], 
+    //     $input['numero'],
+    //     $input['bairro'], 
+    //     $input['cidade'], 
+    //     $input['uf']),
+    //   new CPF($input['cpf']),
+    //   $input['nome'],
+    //   $input['rg'],
+    //   $input['dataNasc'],
+    //   $input['estadoCivil'],
+    //   $input['telefone'],
+    //   new Investimento(
+    //   "6 meses",
+    //   200.00,
+    //   1,
+    //   "5")
+    // );
     try {
-      echo json_encode(
-        array(
-          "status" => "success",
-          "dados" => "Dados cadastrado com sucesso!"
-        ),1);
+    //   echo json_encode(
+    //     array(
+    //       "status" => "success",
+    //       "dados" => "Dados cadastrado com sucesso!"
+    //     ),1);
     } catch (Exception $e) {
       exit($e->getMessage());
     }
