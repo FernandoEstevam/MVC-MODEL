6a7a6cc9b4cb66aea38ef055e87dee8ee9c4d884
Fernando Estevam <fernandoestevam23@gmail.com>
2020-02-27 22:30:56 -0300

    Adicionando try catch no construct da class

--- html/poupemais.com/Src/Lib/Investimento.php
+++ html/poupemais.com/Src/Lib/Investimento.php
@@ -19,12 +19,15 @@ class Investimento extends ValidaDados
   
   public function __construct(string $plano, float $valorPlano, int $aporte, string $dataPrimeiraParcela)
   {
-    $this->plano = $this->converInt($plano);
-    $this->valorPlano = $this->validaInput($valorPlano);
-    $this->dataPrimeiraParcela = $this->validaInput($dataPrimeiraParcela);
-    $this->aporte = $this->validaInput($aporte);
-    $this->vencimento = $this->vencimentos($this->plano, $this->valorPlano, $this->aporte, $this->dataPrimeiraParcela);
-
+    try {
+      $this->plano = $this->converInt($plano);
+      $this->valorPlano = $this->validaInput($valorPlano);
+      $this->dataPrimeiraParcela = $this->validaInput($dataPrimeiraParcela);
+      $this->aporte = $this->validaInput($aporte);
+      $this->vencimento = $this->vencimentos($this->plano, $this->valorPlano, $this->aporte, $this->dataPrimeiraParcela);
+    } catch (Exception $e) {
+      echo $e->getMessage();
+    }
   }
     
   # Cria vencimentos baseado no plano e valor
@@ -40,18 +43,12 @@ class Investimento extends ValidaDados
     $vencimentos = [];
 
     $parcela = ['A','B','C','D','E','F','G','H','I','J','K','L'];
-    
+    echo $nParcelas;
     if($aporte === 2) $nParcelas = 1;
 
-    // if($dataPrimeiraParcela != null){
       $dia = $dataPrimeiraParcela;
       $mes = date("m");
       $ano = date("Y");
-    // } else {
-      // $dia = date("d");
-      // $mes = date("m");
-      // $ano = date("Y");
-    // }
     
     for($x = 0; $x < $nParcelas; $x++){
       $dado = date("Y/m/d",strtotime("+".($x+1)." month",mktime(0, 0, 0,$mes,$dia,$ano)));
