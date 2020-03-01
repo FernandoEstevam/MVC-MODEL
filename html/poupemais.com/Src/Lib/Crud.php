fe686ce709496f91c1d0024911618f3c493864ec
Fernando Estevam <fernandoestevam23@gmail.com>
2020-03-01 12:24:07 -0300

    Adicionando o try catch no metodo prepareExec e criando metodo insert

--- html/poupemais.com/Src/Lib/Crud.php
+++ html/poupemais.com/Src/Lib/Crud.php
@@ -3,23 +3,41 @@
  * class CRUD
  */
 namespace Poupemais\Src\Lib;
+
+use PDOException;
 use Poupemais\Src\Core\ConexaoDB;
+use Poupemais\Src\Core\Erro;
 
 class Crud extends ConexaoDB
 {
-  private $query;
+  private $crud;
 
   # Executa a consulta banco de dados
   private function prepareExec(string $prep, array $exec): void
   {
-    $this->query = $this->conn->prepare($prep);
-    $this->query->execute($exec);
+    try {
+      $this->crud = $this->conn->prepare($prep);
+      try {
+        $this->crud->execute($exec);
+      } catch (PDOException $e) {
+        Erro::setErro($e->getMessage());
+      }
+    } catch (PDOException $e) {
+      Erro::setErro($e->getMessage());
+    }
   }
 
   # Select banco de dados
   protected function selectDB(string $fields, string $table, string $where, array $exec)
   {
     $this->prepareExec("SELECT {$fields} FROM {$table} {$where}", $exec);
-    return $this->query;
+    return $this->crud;
+  }
+
+  # Insert banco de dados
+  protected function insertDB(string $table, string $value, array $exec)
+  {
+    $this->prepareExec("INSERT INTO {$table} VALUES ({$value})", $exec);
+    return $this->crud;
   }
 }
