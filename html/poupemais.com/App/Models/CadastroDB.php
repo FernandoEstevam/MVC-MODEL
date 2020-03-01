47322f65821a6e1ef2bc72393f6b9830cd295e79
Fernando Estevam <fernandoestevam23@gmail.com>
2020-03-01 20:46:42 -0300

    Concluindo o cadastro cliente no banco de dados

--- html/poupemais.com/App/Models/CadastroDB.php
+++ html/poupemais.com/App/Models/CadastroDB.php
@@ -7,13 +7,45 @@ namespace Poupemais\App\Models;
 
 use Poupemais\Src\Core\Erro;
 use Poupemais\Src\Lib\{Cliente,Crud};
-use PDO;
 use PDOException;
+use PDO;
 
 class CadastroDB extends Crud
 {
   private $query;
 
+  # Retorna id
+  private function selectID(string $table, string $condicao, string $value): int
+  {
+    try {
+      $this->query = $this->selectDB(
+        "id",
+        $table,
+        "where {$condicao} = ?",
+        array(
+          $value
+        ),
+      );
+      $id = $this->query->fetch(PDO::FETCH_ASSOC);
+  
+      $row = $this->query->rowCount();
+  
+      if($row == 0) {
+        Erro::setErro("Nenhum registro encontrado!");
+      }
+      return $id['id'];
+    } catch (PDOException $e) {
+      exit(json_encode(
+        array(
+          "status" => "erro",
+          "banco_dados" => $e->getMessage(),
+          "query" => "Erro ao selecionar ID"
+        )
+      ));
+    }
+  }
+
+  # Cadastro usuario
   public function cadastraUsuario(Cliente $cliente): void
   {
     # Inserir roll comit mysql
@@ -32,42 +64,132 @@ class CadastroDB extends Crud
         )
       );
      } catch (PDOException $e) {
-      echo json_encode(
+      exit(json_encode(
         array(
           "status" => "erro",
-          "dados" => $e->getMessage()
+          "banco_dados" => $e->getMessage(),
+          "query" => "Erro ao cadastrar usuario"
         )
-      );
+      ));
     }
   }
 
-  # Retorna id
-  public function selectID(string $table, string $condicao, string $value): int
+  # Cadastro cliente
+  public function cadastraCliente(Cliente $cliente): void
   {
+    # Inserir roll comit mysql
+    if(!is_object($cliente)) {
+      Erro::setErro("Valor passado inválido!");
+    }
+
     try {
-      $this->query = $this->selectDB(
-        "id",
-        $table,
-        "where {$condicao} = ?",
+      # Retorno id 
+      $id_usuario = $this->selectID("usuarios","email",$cliente->getUsuario()->getLogin());
+      # cadastra cliente
+      $this->query = $this->insertDB(
+        "clientes(nome,data_nascimento,cpf,rg,estado_civil,telefone, endereco, numero,complemento, bairro,cep, cidade,uf,id_usuario)",
+        "?,?,?,?,?,?,?,?,?,?,?,?,?,?",
         array(
-          $value
-        ),
-      );
-      $consulta = $this->query->fetch(PDO::FETCH_ASSOC);
-  
-      $row = $this->query->rowCount();
-  
-      if($row == 0) {
-        Erro::setErro("Nenhum registro encontrado!");
-      }
-      return $consulta['id'];
-    } catch (PDOException $e) {
-      echo json_encode(
-        array(
-          "status" => "erro",
-          "dados" => $e->getMessage()
+          $cliente->getNome(),
+          $cliente->getDataNasc(),
+          $cliente->getCPF()->getCPF(),
+          $cliente->getRG(),
+          $cliente->getEstadoCivil(),
+          $cliente->getTelefone(),
+          $cliente->getEndereco()->getlogradouro(),
+          $cliente->getEndereco()->getNumero(),
+          $cliente->getEndereco()->getCompl(),
+          $cliente->getEndereco()->getBairro(),
+          $cliente->getEndereco()->getCep(),
+          $cliente->getEndereco()->getCidade(),
+          $cliente->getEndereco()->getUf(),
+          $id_usuario
         )
       );
+    } catch (PDOException $e) {
+      exit(json_encode(
+        array(
+          "status" => "erro",
+          "banco_dados" => $e->getMessage(),
+          "query" => "Erro ao cadastrar cliente"
+        )
+      ));
     }
-  } 
+  }
+
+  # Cadastro investimento
+  public function cadastraInvestimento(Cliente $cliente): void
+  {
+    # Inserir roll comit mysql
+    if(!is_object($cliente)) {
+      Erro::setErro("Valor passado inválido!");
+    }
+    
+    try {
+      # Retorno id 
+      $id_cliente = $this->selectID("clientes","nome",$cliente->getNome());
+      # cadastra investimento
+      $this->query = $this->insertDB(
+        "investimentos(valor,data_contratacao,situacao,id_cliente,id_plano,id_aporte)",
+        "?,?,?,?,?,?",
+        array(
+          $cliente->getInvestimento()->getValorPlano(),
+          $cliente->getInvestimento()->getDataContratacao(),
+          "aberto",
+          $id_cliente,
+          $cliente->getInvestimento()->getPlano(),
+          $cliente->getInvestimento()->getAporte()
+        )
+      );
+    }catch (PDOException $e) {
+      exit(json_encode(
+        array(
+          "status" => "erro",
+          "banco_dados" => $e->getMessage(),
+          "query" => "Erro ao cadastrar investimento"
+        )
+      ));
+    }
+
+  }
+
+  # Cadastro investimento
+  public function cadastraVencimentos(Cliente $cliente): void
+  {
+    # Inserir roll comit mysql
+    if(!is_object($cliente)) {
+      Erro::setErro("Valor passado inválido!");
+    }
+    
+    try {
+      # Retorno id 
+      $id_investimento = $this->selectID("investimentos","id_cliente",
+        $this->selectID("clientes","nome",$cliente->getNome())
+      );
+
+      # cadastra investimento
+      foreach($cliente->getInvestimento()->getVencimentos() as $vencimento) {
+        $this->query = $this->insertDB(
+          "vencimentos(parcela,vencimento,valor,data_pagamento, situacao, id_investimento)",
+          "?,?,?,?,?,?",
+          array(
+            $vencimento['parcela'],
+            $vencimento['vencimento'],
+            $vencimento['valor'],
+            $vencimento['vencimento'],
+            "aberto",
+            $id_investimento,
+          )
+        );
+      }
+    } catch (PDOException $e) {
+      exit(json_encode(
+        array(
+          "status" => "erro",
+          "banco_dados" => $e->getMessage(),
+          "query" => "Erro ao cadastrar vencimento"
+        )
+      ));
+    }
+  }
 }
